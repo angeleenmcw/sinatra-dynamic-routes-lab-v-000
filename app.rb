3b12962b0259251b9eddc4ad972630d4de547fa5
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-08-18 04:39:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   
   get "/say/:number/:phrase" do
     str = ""
-    @number = params[:number]
+    @number = params[:number].to_i
     @phrase = params[:phrase]
     @number.times {str += "#{:phrase}"}
   end
