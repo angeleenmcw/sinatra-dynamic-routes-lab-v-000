cb10724287b612a8276f5d5996517ce5482a0eca
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-08-18 04:49:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,7 +33,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
-    case 
+    case @operation
     when "substract"
       (@number1-@number2).to_s
     when "add"
